ada84e72fac2c44dc4c045a6c63ee976186e9546
Andrew Kharitonov <askildar@yandex.ru>
2026-03-27 01:01:06 +0500

    Лишний код

--- src/app/services/annotation.service.ts
+++ src/app/services/annotation.service.ts
@@ -5,35 +5,6 @@ import { Article } from "../models/article.model";
 @Injectable({ providedIn: 'root' })
 export class AnnotationService {
 
-  createAnnotation(container: HTMLElement, color: string, note: string): Annotation | null {
-    const selection = window.getSelection();
-    if (!selection || selection.rangeCount === 0) return null;
-
-    const range = selection.getRangeAt(0);
-    const text = range.toString();
-    if (!text.trim()) return null;
-
-    const articleId = 'articleId';
-
-    // Оборачиваем в span
-    const span = document.createElement('span');
-    span.style.borderBottom = `2px solid ${color}`;
-    span.dataset['annotation'] = note;
-    span.dataset['color'] = color;
-    span.classList.add('annotated');
-
-    range.surroundContents(span);
-
-    return {
-      id: crypto.randomUUID(),
-      text: note,
-      color,
-      articleId,
-      note,
-      content: ''
-    };
-  }
-
   getAnnotationById(articleId: string): Annotation {
     const articles = localStorage.getItem('articles');
     let article!: Article;
